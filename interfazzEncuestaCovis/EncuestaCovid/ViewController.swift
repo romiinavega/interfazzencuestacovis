e4661dd5e77d8edaa968f604d17a8fb7c203bce7
romiinavega <53828146+romiinavega@users.noreply.github.com>
2022-03-14 17:48:31 -0700

    Add files via upload

---
+++ interfazzEncuestaCovis/EncuestaCovid/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  EncuestaCovid
+//
+//  Created by Alumno on 3/7/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var alumnoButton: UIButton!
+    @IBOutlet weak var profesorButton: UIButton!
+    @IBOutlet weak var empleadoButton: UIButton!
+    @IBOutlet weak var matriculalbl: UILabel!
+    
+    
+    @IBAction func alumnoIsPressed (Sender: UIButton) {
+        matriculalbl.text = "Matricula"
+        
+    }
+    
+    @IBAction func profesorIsPressed (Sender: UIButton) {
+          matriculalbl.text = "Numero de profesor"
+          
+      }
+    
+    @IBAction func empleadoIsPressed (Sender: UIButton) {
+        matriculalbl.text = "Numero de empleado"
+        
+    }
+
+      
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
